ad9ad081703caf5f9769ebbb65c073f2ac220b70
Malek <stonemokkaks@gmail.com>
2024-06-07 20:57:17 +0300

    fixed problems

--- src/components/Provider/AuthContext.jsx
+++ src/components/Provider/AuthContext.jsx
@@ -1,11 +1,19 @@
 'use client';
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const AuthContext = createContext();
 export const useAuth = () => useContext(AuthContext);
 
 export function AuthProvider({ children }) {
   const [loggedIn, setLoggedIn] = useState(false);
+  const[loading, setLoading] = useState(true);
+  const jwt = typeof window !== 'undefined' && localStorage.getItem('jwt');
+  useEffect(() => {
+    if (jwt) {
+      setLoggedIn(true);
+    }
+    setLoading(false);
+  }, [jwt]);
 
   return (
     <AuthContext.Provider value={{ loggedIn, setLoggedIn }}>
